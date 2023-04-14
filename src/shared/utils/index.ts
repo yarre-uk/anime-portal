5ccf8296959dcee374627163a3835359fe70982c
Dmytro Horbatenko <dm.gorbatenko@gmail.com>
2023-04-14 22:24:18 +0300

    fix: add homepage loader

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -7,3 +7,5 @@ export { default as formatApiError } from './formatApiError';
 export { default as modifiColor } from './colorModifier';
 export type { StringMap } from './TypeUtils';
 export { default as calculateBackgroundColor } from './calculateBackgroundColor';
+export { default as createAnimation } from './createAnimation';
+export { parseLengthAndUnit, cssValue } from './unitConverter';
